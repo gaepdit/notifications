db9ce7783595d4a30ef6e911b7dbe75536be328d
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-07-28 14:04:12 -0400

    Revert "Refactor error logging"
    This reverts commit 5dac50140cf103401a9cb545f1642d096d1efe23.

--- src/Notifications.API/Program.cs
+++ src/Notifications.API/Program.cs
@@ -3,7 +3,7 @@ using Notifications.Database;
 using Notifications.Platform;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.ConfigureRaygunLogging();
+builder.Services.AddRaygun(builder.Configuration);
 builder.Services.BindAppSettings();
 builder.Services.AddApiKeyAuthentication();
 builder.ConfigureDatabase();
