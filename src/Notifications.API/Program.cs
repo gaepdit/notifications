2b8c2d70ea3e1ca03a2c20a61c3913cf5ffeb96c
Doug Waldron <737326+dougwaldron@users.noreply.github.com>
2025-12-01 15:24:49 -0500

    Use ZLogger for logging (#12)

--- src/Notifications.API/Program.cs
+++ src/Notifications.API/Program.cs
@@ -1,8 +1,16 @@
 using Mindscape.Raygun4Net.AspNetCore;
 using Notifications.Database;
 using Notifications.Platform;
+using ZLogger;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders().AddZLoggerConsole(options =>
+{
+    if (builder.Environment.IsDevelopment())
+        options.UsePlainTextFormatter();
+    else
+        options.UseJsonFormatter();
+});
 builder.Services.AddRaygun(builder.Configuration);
 builder.Services.BindAppSettings();
 builder.Services.AddApiKeyAuthentication();
