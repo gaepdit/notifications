9d5272afc84d44afc0beb2f360ca150fcf067d25
Doug Waldron <737326+dougwaldron@users.noreply.github.com>
2026-06-12 08:07:27 -0400

    Remove Raygun (#39)

--- src/Notifications.API/Program.cs
+++ src/Notifications.API/Program.cs
@@ -1,23 +1,13 @@
-using Mindscape.Raygun4Net.AspNetCore;
 using Notifications.Database;
 using Notifications.Platform;
 using ZLogger;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Logging.ClearProviders().AddZLoggerConsole(options =>
-{
-    if (builder.Environment.IsDevelopment())
-        options.UsePlainTextFormatter();
-    else
-        options.UseJsonFormatter();
-});
-builder.Services.AddRaygun(builder.Configuration);
-builder.Services.BindAppSettings();
-builder.Services.AddApiKeyAuthentication();
+builder.Logging.ClearProviders().AddZLoggerConsole(options => options.UseJsonFormatter());
+builder.Services.BindAppSettings().AddApiKeyAuthentication();
 builder.ConfigureDatabase();
 
 var app = builder.Build();
-app.UseRaygun();
 app.MapEndpoints();
 
 await app.BuildDatabaseAsync();
